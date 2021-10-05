5dedda8c5ac6f4e8077be35ec3ae31151cfade00
Raul <raulgradosuperior@gmail.com>
2021-10-05 12:04:21 +0200

    Sesion 02 5/10/2021 -2 h
    Creacion de nuevo de la interfaz de menu principal

--- app/src/main/java/com/example/agendafotografica/MainActivity.java
+++ app/src/main/java/com/example/agendafotografica/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.agendafotografica;
 
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
+import androidx.fragment.app.Fragment;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.agendafotografica.fragments.InicioFragment;
+
+public class MainActivity extends AppCompatActivity implements InicioFragment.OnFragmentInteractionListener {
+
+    Fragment fragmentInicio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentInicio = new InicioFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.contenedorFragments,fragmentInicio).commit();
+    }
+    //@Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }

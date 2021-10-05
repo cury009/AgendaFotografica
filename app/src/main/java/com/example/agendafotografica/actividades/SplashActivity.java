3304cfa920f6031b9155041e5123d820c54590c9
Raul <raulgradosuperior@gmail.com>
2021-10-05 10:03:01 +0200

    Sesion 01 - 4 Oct - 1 h
    Creacion de Splash. (lanza una imagen al abrir la aplicacion y luego la aplicacion)

---
+++ app/src/main/java/com/example/agendafotografica/actividades/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.example.agendafotografica.actividades;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.agendafotografica.MainActivity;
+import com.example.agendafotografica.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
+}

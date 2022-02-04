c57d969ca23177248accc759a9614c0485af88a6
Raul <raulgradosuperior@gmail.com>
2022-02-04 21:21:06 +0100

    Sesion 11 04/02/2022 2h
    - Diseñar CalendarioActivity
    - Crear CardView como botones de CalendarioActivity
    - CardView --> Seleccionar Dia --> Escribe en un textView el dia seleccionado
    - CardView --> Mostrar Evento --> Dirige a MostrarActivity --> (posteriormente consultara los eventos de la base de datos y lo mostrara por pantalla)
    - CardView --> Fijar Evento --> Dirige a InsertarActivity (posteriormente habrá un formulario que conecte con la base de datos
    - CardView --> Borrar Evento --> Dirige a BorrarActivity --> (posteriormente mostrará el evento de ese dia y un botón que podrá borrar el evento de la base de datos

--- app/src/main/java/com/example/agendafotografica/actividades/CalendarioActivity.java
+++ app/src/main/java/com/example/agendafotografica/actividades/CalendarioActivity.java
@@ -1,16 +1,67 @@
 package com.example.agendafotografica.actividades;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.agendafotografica.R;
 
+import java.util.Calendar;
+
 public class CalendarioActivity extends AppCompatActivity {
 
+    private DatePicker datePicker;
+    private TextView dateValueTextView;
+    private CardView seleccionarDia;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendario);
+
+        datePicker = (DatePicker) findViewById(R.id.date_picker);
+        dateValueTextView = (TextView) findViewById(R.id.date_selected_text_view);
+        seleccionarDia = (CardView) findViewById(R.id.cardseleccionarDia);
+
+        // desactivado los dias anteriores a hoy
+        Calendar today = Calendar.getInstance();
+        long now = today.getTimeInMillis();
+        datePicker.setMinDate(now);
+    }
+
+    //método boton selecciona el dia
+    public void seleccionar(View view) {
+        Toast.makeText(getApplicationContext(), "Selecciona el dia", Toast.LENGTH_SHORT).show();
+        dateValueTextView.setText("Dia Seleccionado: " + datePicker.getDayOfMonth() + "/" +(datePicker.getMonth()+1)  + "/" + datePicker.getYear());
+
+
+    }
+    //método botón mostrar evento
+    public void mostrar(View view) {
+        Toast.makeText(getApplicationContext(), "Iniciar Pantalla Mostrar", Toast.LENGTH_SHORT).show();
+        Intent mostrar = new Intent(this, MostrarActivity.class);
+        startActivity(mostrar);
+
+    }
+    //método botón fijar evento
+    public void insertar(View view) {
+        Toast.makeText(getApplicationContext(), "Iniciar Pantalla Insertar", Toast.LENGTH_SHORT).show();
+        Intent insertar = new Intent(this, InsertarActivity.class);
+        startActivity(insertar);
+
+    }
+    //método botón borrar evento
+    public void borrar(View view) {
+        Toast.makeText(getApplicationContext(), "Iniciar Pantlla Borrar", Toast.LENGTH_SHORT).show();
+        Intent borrar = new Intent(this, BorrarActivity.class);
+        startActivity(borrar);
+
     }
 }
---
+++ app/src/main/java/com/example/agendafotografica/actividades/InsertarActivity.java
@@ -0,0 +1,16 @@
+package com.example.agendafotografica.actividades;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.agendafotografica.R;
+
+public class InsertarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insertar);
+    }
+}
